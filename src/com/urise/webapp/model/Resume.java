e6fa6a9eeb12a484711d3957ff648b45ebaf5aa3
PavlenkovG <gs344000@gmail.com>
2023-03-25 21:29:03 +0300

    HW 07

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -12,7 +12,6 @@ public class Resume implements Comparable<Resume> {
 
     // Unique identifier
     private final String uuid;
-
     private final String fullName;
     private final Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
     private final Map<SectionType, Section> sections = new EnumMap<>(SectionType.class);
