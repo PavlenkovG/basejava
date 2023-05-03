884179acbe2cbcc1ded2b99efb2700af9c40b191
PavlenkovG <gs344000@gmail.com>
2023-05-03 15:15:54 +0300

    HW 10 after second review

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -24,29 +24,22 @@ public class DataStreamSerializer implements StreamSerializer {
                 SectionType type = section.getKey();
                 dos.writeUTF(type.name());
                 switch (type) {
-                    case PERSONAL, OBJECTIVE -> {
-                        TextSection textSection = (TextSection) section.getValue();
-                        dos.writeUTF(textSection.getContent());
-                    }
-                    case ACHIEVEMENT, QUALIFICATIONS -> {
-                        ListSection listSection = (ListSection) section.getValue();
-                        writeWithException(listSection.getStrings(), dos, dos::writeUTF);
-                    }
-                    case EXPERIENCE, EDUCATION -> {
-                        CompanySection companySection = (CompanySection) section.getValue();
-                        writeWithException(companySection.getCompany(), dos, (company -> {
-                            dos.writeUTF(company.getTitle());
-                            String website = company.getWebsite();
-                            dos.writeUTF(website == null ? "" : website);
-                            writeWithException(company.getPeriods(), dos, (period -> {
-                                dos.writeUTF(period.getTitle());
-                                dos.writeUTF(period.getStartDate().toString());
-                                dos.writeUTF(period.getEndDate().toString());
-                                String description = period.getDescription();
-                                dos.writeUTF(description == null ? "" : description);
+                    case PERSONAL, OBJECTIVE -> dos.writeUTF(((TextSection) section.getValue()).getContent());
+                    case ACHIEVEMENT, QUALIFICATIONS ->
+                            writeWithException(((ListSection) section.getValue()).getStrings(), dos, dos::writeUTF);
+                    case EXPERIENCE, EDUCATION ->
+                            writeWithException(((CompanySection) section.getValue()).getCompany(), dos, (company -> {
+                                dos.writeUTF(company.getTitle());
+                                String website = company.getWebsite();
+                                dos.writeUTF(website == null ? "" : website);
+                                writeWithException(company.getPeriods(), dos, (period -> {
+                                    dos.writeUTF(period.getTitle());
+                                    dos.writeUTF(period.getStartDate().toString());
+                                    dos.writeUTF(period.getEndDate().toString());
+                                    String description = period.getDescription();
+                                    dos.writeUTF(description == null ? "" : description);
+                                }));
                             }));
-                        }));
-                    }
                 }
             }));
         }
@@ -55,60 +48,60 @@ public class DataStreamSerializer implements StreamSerializer {
     @Override
     public Resume doRead(InputStream is) throws IOException {
         try (DataInputStream dis = new DataInputStream(is)) {
-            String uuid = dis.readUTF();
-            String fullName = dis.readUTF();
-            Resume resume = new Resume(uuid, fullName);
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
-            int sectionSize = dis.readInt();
-            for (int i = 0; i < sectionSize; i++) {
+            Resume resume = new Resume(dis.readUTF(), dis.readUTF());
+            readWithException(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            readWithException(dis, () -> {
                 SectionType type = SectionType.valueOf(dis.readUTF());
                 switch (type) {
                     case PERSONAL, OBJECTIVE -> resume.addSection(type, new TextSection(dis.readUTF()));
                     case ACHIEVEMENT, QUALIFICATIONS -> {
-                        int stringCount = dis.readInt();
                         List<String> strings = new ArrayList<>();
-                        for (int j = 0; j < stringCount; j++) {
-                            strings.add(dis.readUTF());
-                        }
+                        readWithException(dis, () -> strings.add(dis.readUTF()));
                         resume.addSection(type, new ListSection(strings));
                     }
                     case EXPERIENCE, EDUCATION -> {
-                        int organizationCount = dis.readInt();
                         List<Company> companies = new ArrayList<>();
-                        for (int j = 0; j < organizationCount; j++) {
+                        readWithException(dis, () -> {
                             String title = dis.readUTF();
                             String website = dis.readUTF();
-                            int periodCount = dis.readInt();
                             List<Period> periods = new ArrayList<>();
-                            for (int k = 0; k < periodCount; k++) {
+                            readWithException(dis, () -> {
                                 String titlePeriod = dis.readUTF();
                                 LocalDate startDate = LocalDate.parse(dis.readUTF());
                                 LocalDate endDate = LocalDate.parse(dis.readUTF());
                                 String description = dis.readUTF();
                                 periods.add(new Period(titlePeriod, startDate, endDate, description));
-                            }
+                            });
                             companies.add(new Company(title, website, periods));
-                        }
+                        });
                         resume.addSection(type, new CompanySection(companies));
                     }
                 }
-            }
+            });
             return resume;
         }
     }
 
-    private interface myConsumer<T> {
+    private interface myConsumerForWrite<T> {
         void accept(T t) throws IOException;
     }
 
-    private <T> void writeWithException(Collection<T> collection, DataOutputStream dos, myConsumer<T> consumer) throws
-            IOException {
+    private <T> void writeWithException(Collection<T> collection, DataOutputStream dos, myConsumerForWrite<T> consumer)
+            throws IOException {
         dos.writeInt(collection.size());
         for (T element : collection) {
             consumer.accept(element);
+        }
+    }
+
+    private interface myConsumerForRead {
+        void accept() throws IOException;
+    }
+
+    private void readWithException(DataInputStream dis, myConsumerForRead consumer) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            consumer.accept();
         }
     }
 
